37319504e14562e8144c1a5d88548c2c42759864
salgxv <salvarela09@yahoo.com>
2025-03-20 19:09:30 -0700

    new

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -3,6 +3,13 @@ import express from 'express';
 import db from './config/connection.js';
 import routes from './routes/index.js';
 
+
+import mongoose from "mongoose";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+
 await db();
 
 const PORT = process.env.PORT || 3001;
@@ -11,6 +18,13 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.error(err));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../client/dist'));
