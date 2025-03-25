8d937ebbf7ff98461af34fb1ee2d31f5b2079dd1
salgxv <salvarela09@yahoo.com>
2025-03-24 17:08:44 -0700

    mongoose

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -19,12 +19,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-mongoose.connect(process.env.MONGO_URI as string, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.error(err));
+mongoose.connect(process.env.MONGO_URI as string)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("MongoDB connection error:", err));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../client/dist'));
